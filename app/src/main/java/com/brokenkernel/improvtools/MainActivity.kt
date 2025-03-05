0aa66595de4ab8a38e201f69d9b8543a4cb75e0c
Eitan Adler <lists@eitanadler.com>
2025-03-04 16:32:51 -0800

    [intro] add some basic data to begin moving to a giant list

--- app/src/main/java/com/brokenkernel/improvtools/MainActivity.kt
+++ app/src/main/java/com/brokenkernel/improvtools/MainActivity.kt
@@ -1,17 +1,27 @@
 package com.brokenkernel.improvtools
 
 import android.os.Bundle
+import android.view.Surface
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.brokenkernel.improvtools.ui.theme.ImprovToolsTheme
+import androidx.compose.foundation.lazy.items
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -38,10 +48,55 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
     )
 }
 
+data class Message(val author: String, val body: String)
+
+@Composable
+fun MessageCard(msg: Message) {
+    Column {
+        Text(text = msg.author, fontStyle = FontStyle.Italic)
+        Spacer(modifier = Modifier.height(4.dp))
+        Text(text = msg.body)
+    }
+}
+
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     ImprovToolsTheme {
         Greeting("Android")
+    }
+}
+
+
+
+@Composable
+fun Conversation(messages: List<Message>) {
+    LazyColumn {
+        items(messages) { message ->
+            MessageCard(message)
+        }
+    }
+}
+
+@Preview
+@Composable
+fun PreviewConversation() {
+    MaterialTheme {
+        Surface {
+            Conversation(SampleData.conversationSample)
+        }
+    }
+}
+
+
+@Preview
+@Composable
+fun PreviewMessageCard() {
+    MaterialTheme {
+        Surface {
+            MessageCard(
+                msg = Message("Lexi", "Hey, take a look at Jetpack Compose, it's great!")
+            )
+        }
     }
 }
