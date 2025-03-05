c6e263156bab644db023ab9a611a234d30441edf
Eitan Adler <lists@eitanadler.com>
2025-03-04 16:41:05 -0800

    [intro] change sample data to match what I want it to look like

--- app/src/main/java/com/brokenkernel/improvtools/SampleData.kt
+++ app/src/main/java/com/brokenkernel/improvtools/SampleData.kt
@@ -1,75 +1,14 @@
 package com.brokenkernel.improvtools
 
-/**
- * SampleData for Jetpack Compose Tutorial
- */
 object SampleData {
-    // Sample conversation data
     val conversationSample = listOf(
         SuggestionPair(
-            "Lexi",
-            "Test...Test...Test..."
+            "Noun",
+            "Chair"
         ),
         SuggestionPair(
-            "Lexi",
-            """List of Android versions:
-            |Android KitKat (API 19)
-            |Android Lollipop (API 21)
-            |Android Marshmallow (API 23)
-            |Android Nougat (API 24)
-            |Android Oreo (API 26)
-            |Android Pie (API 28)
-            |Android 10 (API 29)
-            |Android 11 (API 30)
-            |Android 12 (API 31)""".trim()
-        ),
-        SuggestionPair(
-            "Lexi",
-            """I think Kotlin is my favorite programming language.
-            |It's so much fun!""".trim()
-        ),
-        SuggestionPair(
-            "Lexi",
-            "Searching for alternatives to XML layouts..."
-        ),
-        SuggestionPair(
-            "Lexi",
-            """Hey, take a look at Jetpack Compose, it's great!
-            |It's the Android's modern toolkit for building native UI.
-            |It simplifies and accelerates UI development on Android.
-            |Less code, powerful tools, and intuitive Kotlin APIs :)""".trim()
-        ),
-        SuggestionPair(
-            "Lexi",
-            "It's available from API 21+ :)"
-        ),
-        SuggestionPair(
-            "Lexi",
-            "Writing Kotlin for UI seems so natural, Compose where have you been all my life?"
-        ),
-        SuggestionPair(
-            "Lexi",
-            "Android Studio next version's name is Arctic Fox"
-        ),
-        SuggestionPair(
-            "Lexi",
-            "Android Studio Arctic Fox tooling for Compose is top notch ^_^"
-        ),
-        SuggestionPair(
-            "Lexi",
-            "I didn't know you can now run the emulator directly from Android Studio"
-        ),
-        SuggestionPair(
-            "Lexi",
-            "Compose Previews are great to check quickly how a composable layout looks like"
-        ),
-        SuggestionPair(
-            "Lexi",
-            "Previews are also interactive after enabling the experimental setting"
-        ),
-        SuggestionPair(
-            "Lexi",
-            "Have you tried writing build.gradle with KTS?"
+            "Verb",
+            "Deduct"
         ),
     )
 }
