860ffcc917d9ed8c9d71bddc0a14c327bb3ba039
Eitan Adler <lists@eitanadler.com>
2025-03-04 18:14:09 -0800

    [intro] remove now unused suggestion pair list

--- app/src/main/java/com/brokenkernel/improvtools/MainActivity.kt
+++ app/src/main/java/com/brokenkernel/improvtools/MainActivity.kt
@@ -6,12 +6,9 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.RowScope
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
@@ -22,7 +19,6 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.brokenkernel.improvtools.ui.theme.ImprovToolsTheme
@@ -46,49 +42,15 @@ class MainActivity : ComponentActivity() {
 
 data class SuggestionPair(val category: String, val audienceIdea: String)
 
-@Composable
-fun SuggestionPairCard(msg: SuggestionPair) {
-    Column {
-        Text(text = msg.category, fontStyle = FontStyle.Italic)
-        Spacer(modifier = Modifier.height(4.dp))
-        Text(text = msg.audienceIdea)
-    }
-}
-
-@Composable
-fun SuggestionPairList(messages: List<SuggestionPair>) {
-    LazyColumn {
-        item {
-            Text(
-                text = "Category",
-                modifier = Modifier
-                    .padding(16.dp)
-                    .fillMaxWidth(),
-                style = MaterialTheme.typography.headlineLarge
-            )
-            Spacer(modifier = Modifier.height(4.dp))
-            Text(
-                text = "Suggestion",
-                modifier = Modifier
-                    .padding(16.dp)
-                    .fillMaxWidth(),
-                style = MaterialTheme.typography.headlineLarge
-            )
-
-        }
-
-        items(messages) { message ->
-            SuggestionPairCard(message)
-        }
-    }
-}
-
 @Preview
 @Composable
 fun PreviewSuggestionPairList() {
     MaterialTheme {
         Surface {
-            SuggestionPairList(SuggestionPairDatum.suggestionPairSamples)
+            SuggestionsScreen(
+                SuggestionPairDatum.suggestionPairSamples,
+                modifier = Modifier.padding(16.dp),
+            )
         }
     }
 }
