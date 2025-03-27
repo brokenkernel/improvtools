7205da5fe1c4914a9a400da4b61d20be29ad03bf
Eitan Adler <lists@eitanadler.com>
2025-03-27 14:49:30 -0700

    [lib] move TableCell into lib too

--- app/src/main/java/com/brokenkernel/improvtools/MainActivity.kt
+++ app/src/main/java/com/brokenkernel/improvtools/MainActivity.kt
@@ -6,10 +6,7 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.activity.viewModels
 import androidx.compose.foundation.background
-import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.RowScope
-import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
@@ -17,11 +14,9 @@ import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.Lifecycle
@@ -29,6 +24,7 @@ import androidx.lifecycle.lifecycleScope
 import androidx.lifecycle.repeatOnLifecycle
 import com.brokenkernel.improvtools.suggestionGenerator.data.model.SuggestionDatum
 import com.brokenkernel.improvtools.suggestionGenerator.presentation.ClickableTableCell
+import com.brokenkernel.improvtools.suggestionGenerator.presentation.TableCell
 import com.brokenkernel.improvtools.suggestionGenerator.presentation.viewmodel.SuggestionsActivityViewModel
 import com.brokenkernel.improvtools.ui.theme.ImprovToolsTheme
 import kotlinx.coroutines.launch
@@ -69,23 +65,7 @@ fun PreviewSuggestionPairList() {
     }
 }
 
-@Composable
-fun RowScope.TableCell(
-    text: String,
-    weight: Float,
-    style: TextStyle,
-) {
-    Text(
-        text = text,
-        modifier = Modifier
-            .border(1.dp, MaterialTheme.colorScheme.tertiary)
-            .fillMaxHeight()
-            .weight(weight)
-            .padding(8.dp)
-            .fillMaxWidth(),
-        style = style,
-    )
-}
+
 
 
 
--- app/src/main/java/com/brokenkernel/improvtools/suggestionGenerator/presentation/TableCell.kt
+++ app/src/main/java/com/brokenkernel/improvtools/suggestionGenerator/presentation/TableCell.kt
@@ -4,6 +4,7 @@ import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.RowScope
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
@@ -11,6 +12,24 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.unit.dp
+
+@Composable
+fun RowScope.TableCell(
+    text: String,
+    weight: Float,
+    style: TextStyle,
+) {
+    Text(
+        text = text,
+        modifier = Modifier
+            .border(1.dp, MaterialTheme.colorScheme.tertiary)
+            .fillMaxHeight()
+            .weight(weight)
+            .padding(8.dp)
+            .fillMaxWidth(),
+        style = style,
+    )
+}
 
 @Composable
 fun RowScope.ClickableTableCell(
