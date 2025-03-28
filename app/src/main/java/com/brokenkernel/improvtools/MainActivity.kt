0559d2617cbb1f9017d1e251484b1128de50fcc4
Eitan Adler <lists@eitanadler.com>
2025-03-28 03:06:14 -0700

    [suggestions] use scaffold for action bar instead of theme

--- app/src/main/java/com/brokenkernel/improvtools/MainActivity.kt
+++ app/src/main/java/com/brokenkernel/improvtools/MainActivity.kt
@@ -10,12 +10,16 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.BottomAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarColors
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.brokenkernel.improvtools.suggestionGenerator.presentation.view.SuggestionsScreen
 import com.brokenkernel.improvtools.suggestionGenerator.presentation.viewmodel.SuggestionScreenViewModel
@@ -50,13 +54,14 @@ fun OuterContentForSuggestionsScreen(
         Scaffold(
             topBar = {
                 TopAppBar(
-                    title = { Text("hello") } ,
+                    colors = TopAppBarDefaults.topAppBarColors(
+                        containerColor = MaterialTheme.colorScheme.primaryContainer,
+                        titleContentColor = MaterialTheme.colorScheme.primary,
+                    ),
+                    title = { Text(stringResource(R.string.suggestions_activity_title)) } ,
                 )
             },
             bottomBar = {
-                BottomAppBar() {
-                    Text("Good Bye")
-                }
             }
         ) { innerPadding ->
             Surface(modifier = Modifier.fillMaxSize()
