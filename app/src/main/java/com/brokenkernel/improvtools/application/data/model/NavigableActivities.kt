3e2154ac4090bfe7094dcd6dd4d5f71bbd9e5a7f
Eitan Adler <lists@eitanadler.com>
2025-03-28 17:36:17 -0400

    [application] remove duplicative title/title from sealed and enum

--- app/src/main/java/com/brokenkernel/improvtools/application/data/model/NavigableActivities.kt
+++ app/src/main/java/com/brokenkernel/improvtools/application/data/model/NavigableActivities.kt
@@ -1,46 +1,42 @@
 package com.brokenkernel.improvtools.application.data.model
 
-import android.app.Activity
+import androidx.annotation.StringRes
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Home
-import androidx.compose.material.icons.filled.Star
 import androidx.compose.material.icons.outlined.Info
 import androidx.compose.material.icons.outlined.Settings
 import androidx.compose.ui.graphics.vector.ImageVector
 import com.brokenkernel.improvtools.R
 
-//typealias ResourceId = Int // TODO consider inline value class instead
 
 internal sealed class ActiveScreen(
     val icon: ImageVector,
     val contentDescription: String,
-    val title: String,
     val route: String,
 ) {
     object SuggestionGenerator: ActiveScreen(
         icon = Icons.Default.Home, // TODO - placeholder
-        contentDescription = "Go To Suggestion Generator=",
-        title = "Suggestion Generator",
+        contentDescription = "Go To Suggestion Generator",
         route = "suggestion_generator_screen",
     )
 
     object About: ActiveScreen(
         icon = Icons.Outlined.Info, // TODO - placeholder
         contentDescription = "Go to about screen",
-        title = "About",
         route = "about_screen"
     )
 
     object Settings: ActiveScreen(
         icon = Icons.Outlined.Settings,
         contentDescription = "Go to settings screen",
-        title = "Settings",
         route = "settings_screen"
     )
 }
 
 
-internal enum class NavigableActivities(internal val titleResource: Int, internal val activeScreen: ActiveScreen) {
+internal enum class NavigableActivities(
+    @StringRes internal val titleResource: Int,
+    internal val activeScreen: ActiveScreen) {
     SuggestionGenerator(
         titleResource = R.string.suggestions_activity_title,
         activeScreen = ActiveScreen.SuggestionGenerator,
