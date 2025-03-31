c8ce77007c550dfe2e5e5f35da405bbed8fadd5a
Eitan Adler <lists@eitanadler.com>
2025-03-30 23:36:42 -0400

    [timer] create basic outline of timer app

--- app/src/main/java/com/brokenkernel/improvtools/timer/presentation/view/TimerScreen.kt
+++ app/src/main/java/com/brokenkernel/improvtools/timer/presentation/view/TimerScreen.kt
@@ -1,9 +1,25 @@
 package com.brokenkernel.improvtools.timer.presentation.view
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material3.ElevatedButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 
 @Composable
 fun TimerScreen() {
-    Text("timer")
+    Column {
+        Text("Time Remaining", style = MaterialTheme.typography.displayLarge)
+        ElevatedButton(onClick = {}, enabled = false) {
+            Text("start")
+        }
+        TextButton(onClick = {}, enabled = false) {
+            Text("half time")
+        }
+        TextButton(onClick = {}, enabled = false) {
+            Text("reset to 60s")
+        }
+        Text("TODO: figure out how to set initial time in general")
+    }
 }
