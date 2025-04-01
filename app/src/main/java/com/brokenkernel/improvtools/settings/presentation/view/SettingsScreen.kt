3185d751cb1be36bad761b546342a124a3935c58
Eitan Adler <lists@eitanadler.com>
2025-04-01 13:06:57 -0400

    [settings] swap checkbox to Switch for live settings

--- app/src/main/java/com/brokenkernel/improvtools/settings/presentation/view/SettingsScreen.kt
+++ app/src/main/java/com/brokenkernel/improvtools/settings/presentation/view/SettingsScreen.kt
@@ -1,13 +1,15 @@
 package com.brokenkernel.improvtools.settings.presentation.view
 
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.material3.Checkbox
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Switch
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.brokenkernel.improvtools.R
@@ -20,21 +22,25 @@ internal fun SettingsScreen(
 
     val uiState by viewModel.uiState.collectAsState()
 
-    // TODO: figure out how to make this available to the other features
-
     Column {
         Text(
             stringResource(R.string.suggestions_activity_title),
             style = MaterialTheme.typography.titleLarge
         )
         Row() {
-            Text("Allow Reuse")
-            Checkbox(
-                checked = uiState.shouldReuseSuggestions,
-                onCheckedChange = {
-                    viewModel.onClickUpdateShouldReuseSuggestions(it)
-                },
-            )
+            Box(modifier = Modifier.weight(.8f)) {
+                Text(
+                    stringResource(R.string.settings_allow_reuse),
+                )
+            }
+            Box() {
+                Switch(
+                    checked = uiState.shouldReuseSuggestions,
+                    onCheckedChange = {
+                        viewModel.onClickUpdateShouldReuseSuggestions(it)
+                    },
+                )
+            }
         }
     }
 }
