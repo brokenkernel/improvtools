5290daee0f0db20a30cea11ffc421459807193bd
Eitan Adler <lists@eitanadler.com>
2025-04-02 14:11:01 -0700

    [fastlane] add missing page screenshots

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -22,7 +22,7 @@ android {
         applicationId = "com.brokenkernel.improvtools"
         minSdk = 34
         targetSdk = 36
-        versionCode = 8
+        versionCode = 9
         versionName = "0.0.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
--- app/src/androidTest/java/com/brokenkernel/improvtools/deployment/screenshots/ScreenshotGenerationTest.kt
+++ app/src/androidTest/java/com/brokenkernel/improvtools/deployment/screenshots/ScreenshotGenerationTest.kt
@@ -1,28 +1,14 @@
 package com.brokenkernel.improvtools.deployment.screenshots
 
 import androidx.annotation.StringRes
-import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.test.SemanticsMatcher
 import androidx.compose.ui.test.hasContentDescription
-import androidx.compose.ui.test.hasText
 import androidx.compose.ui.test.junit4.ComposeContentTestRule
 import androidx.compose.ui.test.junit4.createComposeRule
-import androidx.compose.ui.test.onRoot
 import androidx.compose.ui.test.performClick
-import androidx.compose.ui.test.printToLog
-import androidx.lifecycle.Lifecycle
-import androidx.navigation.compose.ComposeNavigator
-import androidx.navigation.testing.TestNavHostController
-import androidx.test.core.app.ActivityScenario.launch
-import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.platform.app.InstrumentationRegistry.getInstrumentation
-import com.brokenkernel.improvtools.MainActivity
 import com.brokenkernel.improvtools.OuterContentForMasterScreen
 import com.brokenkernel.improvtools.R
-import com.brokenkernel.improvtools.application.data.model.NavigableRoute
-import com.brokenkernel.improvtools.application.presentation.view.DrawerNavGraph
-import com.fasterxml.jackson.annotation.JsonTypeInfo.Id
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
@@ -52,35 +38,82 @@ class ScreenshotGenerationTest {
 //            DrawerNavGraph(drawerNavController = navController)
             OuterContentForMasterScreen()
         }
+    }
+
+
+    @Test
+    fun takeSettingsPageScreenshot() {
+        composeTestRule.onNode(
+            hasContentDescription(getString(R.string.navigation_app_menu))
+        )
+            .performClick()
+        composeTestRule.waitForIdle()
+        composeTestRule.onNode(
+            hasContentDescription(getString(R.string.go_to_settings_screen))
+        )
+            .performClick()
+        composeTestRule.waitForIdle()
+
+
+
+        Screengrab.screenshot("settings_screen_baseline")
 
     }
-//
-//    @Test
-//    fun takeSuggestionsPageScreenshot() {
-//        activityScenarioRule.scenario.onActivity { activity ->
-//            navController.navigate(NavigableRoute.SuggestionGeneratorRoute)
-//            Screengrab.setDefaultScreenshotStrategy(UiAutomatorScreenshotStrategy())
-//            Screengrab.screenshot("suggestionGenerator_screen_baseline")
-//        }
-//    }
-//
-//    @Test
-//    fun takeSettingsPageScreenshot() {
-//        activityScenarioRule.scenario.onActivity { activity ->
-//            navController.navigate(NavigableRoute.SettingsRoute)
-//            Screengrab.setDefaultScreenshotStrategy(UiAutomatorScreenshotStrategy())
-//            Screengrab.screenshot("settings_screen_baseline")
-//        }
-//    }
-//
-//    @Test
-//    fun takeTimerPageScreenshot() {
-//        activityScenarioRule.scenario.onActivity { activity ->
-//            navController.navigate(NavigableRoute.TimerRoute)
-//            Screengrab.setDefaultScreenshotStrategy(UiAutomatorScreenshotStrategy())
-//            Screengrab.screenshot("timer_screen_baseline")
-//        }
-//    }
+
+
+    @Test
+    fun takeSuggestionsPageScreenshot() {
+        composeTestRule.onNode(
+            hasContentDescription(getString(R.string.navigation_app_menu))
+        )
+            .performClick()
+        composeTestRule.waitForIdle()
+        composeTestRule.onNode(
+            hasContentDescription(getString(R.string.go_to_suggestion_generator))
+        )
+            .performClick()
+        composeTestRule.waitForIdle()
+
+
+
+        Screengrab.screenshot("suggestions_screen_baseline")
+
+    }
+
+    @Test
+    fun takeTimerPageScreenshot() {
+        composeTestRule.onNode(
+            hasContentDescription(getString(R.string.navigation_app_menu))
+        )
+            .performClick()
+        composeTestRule.waitForIdle()
+        composeTestRule.onNode(
+            hasContentDescription(getString(R.string.go_to_timer_screen))
+        )
+            .performClick()
+        composeTestRule.waitForIdle()
+
+
+        Screengrab.screenshot("timer_screen_baseline")
+    }
+
+
+    @Test
+    fun takeAboutPageScreenshot() {
+        composeTestRule.onNode(
+            hasContentDescription(getString(R.string.navigation_app_menu))
+        )
+            .performClick()
+        composeTestRule.waitForIdle()
+        composeTestRule.onNode(
+            hasContentDescription(getString(R.string.go_to_help_and_feedback_screen))
+        )
+            .performClick()
+        composeTestRule.waitForIdle()
+
+
+        Screengrab.screenshot("about_screen_baseline")
+    }
 
     @Test
     fun testMenuOnTopOfStartPageScreen() {
@@ -88,23 +121,8 @@ class ScreenshotGenerationTest {
             hasContentDescription(getInstrumentation().targetContext.getString(R.string.navigation_app_menu))
         ).performClick()
         composeTestRule.waitForIdle()
-        Screengrab.setDefaultScreenshotStrategy(UiAutomatorScreenshotStrategy())
+
         Screengrab.screenshot("menu_on_top_of_start_page")
-    }
-
-    @Test
-    fun takeAboutPageScreenshot() {
-        composeTestRule.onNode(
-            hasContentDescription(getString(R.string.navigation_app_menu)))
-            .performClick()
-        composeTestRule.waitForIdle()
-        composeTestRule.onNode(
-            hasContentDescription(getString(R.string.go_to_help_and_feedback_screen)))
-            .performClick()
-        composeTestRule.waitForIdle()
-
-
-        Screengrab.screenshot("about_screen_baseline")
     }
 
     fun getString(@StringRes id: Int): String {
