3f953d9e62f8c4cbb5242134e0011586c99110e5
Eitan Adler <lists@eitanadler.com>
2025-04-03 00:17:09 -0700

    [suggestions] add icon for reset button on suggestions screen

--- app/src/main/java/com/brokenkernel/improvtools/suggestionGenerator/presentation/view/SuggestionsScreen.kt
+++ app/src/main/java/com/brokenkernel/improvtools/suggestionGenerator/presentation/view/SuggestionsScreen.kt
@@ -5,19 +5,25 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.IntrinsicSize
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ChangeCircle
+import androidx.compose.material3.FilledTonalButton
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -105,8 +111,18 @@ internal fun SuggestionsScreenFullyLoaded(viewModel: SuggestionScreenViewModel =
                 .fillMaxWidth()
                 .fillMaxHeight()
         ) {
-            OutlinedButton(onClick = { viewModel.resetAllCategories() }) {
-                Text(text = stringResource(R.string.suggestions_reset_all))
+            FilledTonalButton(
+                onClick = { viewModel.resetAllCategories() },
+
+                ) {
+                Row {
+                    Icon(
+                        Icons.Filled.ChangeCircle,
+                        contentDescription = stringResource(R.string.suggestions_reset_all)
+                    )
+                    Spacer(modifier = Modifier.padding(dimensionResource(R.dimen.padding_icon_text_for_button)))
+                    Text(text = stringResource(R.string.suggestions_reset_all))
+                }
             }
         }
     }
