3546216f217630456998cf0aa02890d404ed5c01
Eitan Adler <lists@eitanadler.com>
2025-04-04 12:23:18 -0700

    [fastlane] bump version code to 25

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -29,7 +29,7 @@ android {
         applicationId = "com.brokenkernel.improvtools"
         minSdk = 34
         targetSdk = 36
-        versionCode = 24
+        versionCode = 25
         versionName = "0.0.$versionCode"
 
         testInstrumentationRunner = "com.brokenkernel.improvtools.infrastructure.ImprovToolsTestRunner"
