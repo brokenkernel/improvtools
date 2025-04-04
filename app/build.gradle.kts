4f0dbcaeb7c14105f97e0c21d9eb3522b6deecce
Eitan Adler <lists@eitanadler.com>
2025-04-04 13:04:47 -0700

    [fastlane] temporarily disable automated submission due to google error

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -29,7 +29,7 @@ android {
         applicationId = "com.brokenkernel.improvtools"
         minSdk = 34
         targetSdk = 36
-        versionCode = 30
+        versionCode = 31
         versionName = "0.0.$versionCode"
 
         testInstrumentationRunner = "com.brokenkernel.improvtools.infrastructure.ImprovToolsTestRunner"
