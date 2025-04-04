0e27a52653a58d61a203b95c76c187458e9a71a2
Eitan Adler <lists@eitanadler.com>
2025-04-04 03:41:18 -0700

    [tips] optimise imports

--- app/src/main/java/com/brokenkernel/improvtools/tipsandadvice/presentation/view/TipsAndAdviceScreen.kt
+++ app/src/main/java/com/brokenkernel/improvtools/tipsandadvice/presentation/view/TipsAndAdviceScreen.kt
@@ -1,13 +1,10 @@
 package com.brokenkernel.improvtools.tipsandadvice.presentation.view
 
-import android.text.Selection
 import android.widget.TextView
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxHeight
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.text.selection.SelectionContainer
 import androidx.compose.material.icons.Icons
@@ -17,16 +14,16 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.viewinterop.AndroidView
 import androidx.core.text.HtmlCompat
 import androidx.hilt.navigation.compose.hiltViewModel
-import com.brokenkernel.improvtools.tipsandadvice.presentation.viewmodel.TipsAndAdviceViewModel
-import androidx.compose.runtime.getValue
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.res.dimensionResource
-import androidx.compose.ui.res.stringResource
 import com.brokenkernel.improvtools.R
+import com.brokenkernel.improvtools.tipsandadvice.presentation.viewmodel.TipsAndAdviceViewModel
 
 
 @Composable
