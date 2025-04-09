4cff76224c0ae73343416d9528568cd141dcf535
Eitan Adler <lists@eitanadler.com>
2025-04-09 16:32:46 -0700

    [app] use NavigationSuiteScaffold instead of Scaffold.bottomBar
    I'm not actually sure what the reco is. but this seems to be closer to better.

--- app/src/main/java/com/brokenkernel/improvtools/application/presentation/view/ImprovToolsScaffold.kt
+++ app/src/main/java/com/brokenkernel/improvtools/application/presentation/view/ImprovToolsScaffold.kt
@@ -16,6 +16,8 @@ import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.material3.adaptive.navigationsuite.NavigationSuiteScaffold
+import androidx.compose.material3.adaptive.navigationsuite.NavigationSuiteScope
 import androidx.compose.material3.rememberTopAppBarState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.State
@@ -41,77 +43,103 @@ internal fun ImprovToolsScaffold(
     val snackbarHostState = remember { SnackbarHostState() }
     var moreMenuExpandedState: Boolean by remember { mutableStateOf(false) }
 
-
-    // TODO: deal with NavigationSuiteScaffold
-    Scaffold(
-        topBar = {
-            CenterAlignedTopAppBar(
-                colors = TopAppBarDefaults.topAppBarColors(
-                    containerColor = MaterialTheme.colorScheme.primaryContainer,
-                    titleContentColor = MaterialTheme.colorScheme.primary,
-                ),
-                title = { Text(stringResource(currentNavigableScreen.value.titleResource)) },
-                scrollBehavior = TopAppBarDefaults
-                    .exitUntilCollapsedScrollBehavior(
-                        rememberTopAppBarState()
-                    ),
-                navigationIcon = {
-                    IconButton(onClick = navMenuButtonPressedCallback) {
-                        Icon(
-                            imageVector = Icons.Default.Menu,
-                            contentDescription = stringResource(R.string.navigation_app_menu)
-                        )
-                    }
-                },
-                actions = {
-                    IconButton(
-                        onClick = {
-                            moreMenuExpandedState = !moreMenuExpandedState
-                        },
-                        enabled = currentNavigableScreen.value.shouldShowExtraMenu,
-                    ) {
-                        if (currentNavigableScreen.value.shouldShowExtraMenu) {
-                            Icon(
-                                imageVector = Icons.Filled.MoreVert,
-                                contentDescription = stringResource(R.string.navigation_open_screen_specific_menu)
-                            )
-                            when (currentNavigableScreen.value) {
-                                NavigableScreens.SuggestionGenerator -> SuggestionsScreenMenu(
-                                    expanded = moreMenuExpandedState,
-                                    onDismiss = {
-                                        moreMenuExpandedState = !moreMenuExpandedState
-                                    }
-                                )
-
-                                NavigableScreens.TipsAndAdvice -> TipsAndAdviceMenu(
-                                    expanded = moreMenuExpandedState,
-                                    onDismiss = {
-                                        moreMenuExpandedState = !moreMenuExpandedState
-                                    }
-                                )
-
-                                else -> {
-                                    // There is no menu, so we're good.
-                                }
-                            }
-                        }
-
-                    }
-                },
-            )
-        },
-        bottomBar = {
-            ImprovToolsBottomBar(currentNavigableScreen, doNavigateToNavigableScreen)
-        },
-        snackbarHost = {
-            SnackbarHost(hostState = snackbarHostState)
-        },
-    ) { innerPadding ->
-        Surface(
-            modifier = Modifier
-                .fillMaxSize()
-                .padding(innerPadding),
-            content = content,
+    // TODO: maybe do as loop rather than extension method?
+    fun NavigationSuiteScope.simpleNavigableScreen(it: NavigableScreens) {
+        return item(
+            icon = {
+                Icon(
+                    it.icon,
+                    contentDescription = stringResource(it.contentDescription)
+                )
+            },
+            label = {
+                Text(stringResource(it.titleResource))
+            },
+            selected = (currentNavigableScreen.value == it),
+            onClick = {
+                doNavigateToNavigableScreen(it)
+            }
         )
     }
+
+
+    NavigationSuiteScaffold(
+        navigationSuiteItems = {
+            simpleNavigableScreen(NavigableScreens.SuggestionGenerator)
+            simpleNavigableScreen(NavigableScreens.Timer)
+        },
+    ) {
+        Scaffold(
+            topBar = {
+                CenterAlignedTopAppBar(
+                    colors = TopAppBarDefaults.topAppBarColors(
+                        containerColor = MaterialTheme.colorScheme.primaryContainer,
+                        titleContentColor = MaterialTheme.colorScheme.primary,
+                    ),
+                    title = { Text(stringResource(currentNavigableScreen.value.titleResource)) },
+                    scrollBehavior = TopAppBarDefaults
+                        .exitUntilCollapsedScrollBehavior(
+                            rememberTopAppBarState()
+                        ),
+                    navigationIcon = {
+                        IconButton(onClick = navMenuButtonPressedCallback) {
+                            Icon(
+                                imageVector = Icons.Default.Menu,
+                                contentDescription = stringResource(R.string.navigation_app_menu)
+                            )
+                        }
+                    },
+                    actions = {
+                        IconButton(
+                            onClick = {
+                                moreMenuExpandedState = !moreMenuExpandedState
+                            },
+                            enabled = currentNavigableScreen.value.shouldShowExtraMenu,
+                        ) {
+                            if (currentNavigableScreen.value.shouldShowExtraMenu) {
+                                Icon(
+                                    imageVector = Icons.Filled.MoreVert,
+                                    contentDescription = stringResource(R.string.navigation_open_screen_specific_menu)
+                                )
+                                when (currentNavigableScreen.value) {
+                                    NavigableScreens.SuggestionGenerator -> SuggestionsScreenMenu(
+                                        expanded = moreMenuExpandedState,
+                                        onDismiss = {
+                                            moreMenuExpandedState = !moreMenuExpandedState
+                                        }
+                                    )
+
+                                    NavigableScreens.TipsAndAdvice -> TipsAndAdviceMenu(
+                                        expanded = moreMenuExpandedState,
+                                        onDismiss = {
+                                            moreMenuExpandedState = !moreMenuExpandedState
+                                        }
+                                    )
+
+                                    else -> {
+                                        // There is no menu, so we're good.
+                                    }
+                                }
+                            }
+
+                        }
+                    },
+                )
+            },
+//            bottomBar = {
+//                ImprovToolsBottomBar(currentNavigableScreen, doNavigateToNavigableScreen)
+//            },
+            snackbarHost = {
+                SnackbarHost(hostState = snackbarHostState)
+            },
+        ) { innerPadding ->
+            Surface(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(innerPadding),
+                content = content,
+            )
+        }
+    }
+
 }
