67a0e8638877c95d8e574edd2936afa9564b8329
Eitan Adler <lists@eitanadler.com>
2025-04-13 11:43:08 -0700

    [people] begin adding details about people.
    - also reorg people page to allow for links, etc.
    - add more people

--- app/src/main/java/com/brokenkernel/improvtools/encyclopaedia/data/model/PeopleDaatum.kt
+++ app/src/main/java/com/brokenkernel/improvtools/encyclopaedia/data/model/PeopleDaatum.kt
@@ -1,10 +1,15 @@
 package com.brokenkernel.improvtools.encyclopaedia.data.model
 
+import android.net.Uri
 import androidx.annotation.RestrictTo
+import androidx.core.net.toUri
+
+// TODO: add link to wikipedia for others
+// TODO: use link to wikipedia
 
 @RestrictTo(RestrictTo.Scope.LIBRARY_GROUP)
-internal enum class PeopleDaatum(val personName: String, val knownFor: String) {
-    ViolaSpolin("Viola Spolin", "Theater Games"),
+internal enum class PeopleDaatum(val personName: String, val knownFor: String, val wikipediaLink: Uri? = null) {
+    ViolaSpolin("Viola Spolin", "Theater Games", "https://en.wikipedia.org/wiki/Viola_Spolin".toUri()),
     KeithJohnstone("Keith Johnstone", "Impro & Theatresports"),
     PaulSills("Paul Sills", "The Second City"),
     SusanMessing("Susan Messing", " Annoyance Theater and iO Theater"),
@@ -25,4 +30,13 @@ internal enum class PeopleDaatum(val personName: String, val knownFor: String) {
     AugustoBoal("Augusto Boal", "Theatre of the Oppressed"),
     RuthZaporah("Ruth Zaporah", "Action Theatre"),
     WilliamHall("William Hall", "BATS Improv"),
+    ;
+
+    init {
+        // sanity check/validation
+        if (this.wikipediaLink != null) {
+            require(this.wikipediaLink.scheme == "https")
+        }
+    }
+
 }
--- app/src/main/java/com/brokenkernel/improvtools/encyclopaedia/presentation/view/PeopleTab.kt
+++ app/src/main/java/com/brokenkernel/improvtools/encyclopaedia/presentation/view/PeopleTab.kt
@@ -1,9 +1,13 @@
 package com.brokenkernel.improvtools.encyclopaedia.presentation.view
 
+import android.app.ActivityOptions
+import android.content.Intent
+import android.content.res.Resources
 import android.icu.text.Collator
 import android.icu.text.SearchIterator.DONE
 import android.icu.text.StringSearch
 import android.icu.util.ULocale
+import android.os.Bundle
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.WindowInsets
@@ -16,6 +20,7 @@ import androidx.compose.material.icons.filled.Search
 import androidx.compose.material.icons.outlined.Person
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.ListItem
 import androidx.compose.material3.SearchBar
 import androidx.compose.material3.SearchBarDefaults
@@ -27,11 +32,16 @@ import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.semantics.isTraversalGroup
 import androidx.compose.ui.semantics.semantics
 import androidx.compose.ui.semantics.traversalIndex
 import androidx.compose.ui.text.intl.Locale
 import androidx.compose.ui.unit.dp
+import androidx.core.content.ContextCompat.startActivity
+import com.brokenkernel.improvtools.R
 import com.brokenkernel.improvtools.application.presentation.view.verticalColumnScrollbar
 import com.brokenkernel.improvtools.encyclopaedia.data.model.PeopleDaatum
 import java.text.StringCharacterIterator
@@ -125,10 +135,32 @@ internal fun PeopleTab() {
                                         contentDescription = "Person",
                                     )
                                 },
-                                trailingContent = { Text(it.knownFor) }
+                                overlineContent = { Text(it.knownFor) },
+                                trailingContent = {
+                                    if (it.wikipediaLink != null) {
+                                        val context = LocalContext.current
+                                        IconButton(onClick = {
+                                            // webview.setWebViewClient(new WebViewClient());
+                                            val browserIntent = Intent(Intent.ACTION_VIEW, it.wikipediaLink);
+                                            context.startActivity(browserIntent)
+                                        }) {
+                                            Icon(
+                                                painterResource(R.drawable.logo_wikipedia),
+                                                contentDescription = stringResource(R.string.wikipedia),
+                                            )
+                                        }
+                                    }
+                                }
                             )
                         }
                     }
+                ListItem(
+                    headlineContent = { Text("headline") },
+                    leadingContent = { Text("leading") },
+                    trailingContent = { Text("trailing") },
+                    supportingContent = { Text("supporting") },
+                    overlineContent = { Text("overline") },
+                )
             }
         }
     }
