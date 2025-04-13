992801fe62cc48baaae0c9e7b940fdf084af945b
Eitan Adler <lists@eitanadler.com>
2025-04-12 19:30:49 -0700

    [suggestions] remove header from Suggestions screen

--- app/src/main/java/com/brokenkernel/improvtools/suggestionGenerator/presentation/view/SuggestionsScreen.kt
+++ app/src/main/java/com/brokenkernel/improvtools/suggestionGenerator/presentation/view/SuggestionsScreen.kt
@@ -1,8 +1,6 @@
 package com.brokenkernel.improvtools.suggestionGenerator.presentation.view
 
 import androidx.compose.foundation.ScrollState
-import androidx.compose.foundation.background
-import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
@@ -22,7 +20,6 @@ import androidx.compose.material3.FilledTonalButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.ListItem
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.State
@@ -31,7 +28,6 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.brokenkernel.improvtools.R
 import com.brokenkernel.improvtools.application.data.model.NavigableRoute
@@ -44,37 +40,11 @@ internal fun SuggestionsScreen(
     viewModel: SuggestionScreenViewModel = hiltViewModel(),
     onNavigateToRoute: (NavigableRoute) -> Unit,
 ) {
-// TODO: add info icon - general
-
     Column(
         horizontalAlignment = Alignment.CenterHorizontally,
         modifier = Modifier
             .fillMaxSize()
     ) {
-        // Header
-        Row(
-            modifier = Modifier
-                .fillMaxWidth()
-                .weight(1f)
-        ) {
-            Text(
-                text = stringResource(R.string.suggestions_title_category),
-                modifier = Modifier
-                    .background(MaterialTheme.colorScheme.surfaceVariant)
-                    .border(1.dp, MaterialTheme.colorScheme.tertiary)
-                    .fillMaxWidth(.3f),
-                style = MaterialTheme.typography.titleLarge,
-            )
-            Text(
-                text = stringResource(R.string.suggestions_audience_idea),
-                modifier = Modifier
-                    .background(MaterialTheme.colorScheme.surfaceVariant)
-                    .border(1.dp, MaterialTheme.colorScheme.tertiary)
-                    .fillMaxWidth(),
-                style = MaterialTheme.typography.titleLarge,
-            )
-        }
-
         Row(
             modifier = Modifier
                 .fillMaxWidth()
