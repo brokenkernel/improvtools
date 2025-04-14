e2c4354fc9dda31510380ad2a9f370b50077f526
Eitan Adler <lists@eitanadler.com>
2025-04-14 00:15:38 -0700

    [fastlane] bump version code to 116

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -49,7 +49,7 @@ android {
         applicationId = "com.brokenkernel.improvtools"
         minSdk = 28
         targetSdk = 36
-        versionCode = 115
+        versionCode = 116
         versionName = "0.0.$versionCode"
 
         testInstrumentationRunner = "com.brokenkernel.improvtools.infrastructure.ImprovToolsTestRunner"
