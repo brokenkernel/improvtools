070b09e72527003d59fa1b9bff6a237c2b49217c
Eitan Adler <lists@eitanadler.com>
2025-04-27 01:39:32 -0700

    [all] rm unused imports

--- app/src/main/java/com/brokenkernel/improvtools/application/presentation/view/LibrariesTab.kt
+++ app/src/main/java/com/brokenkernel/improvtools/application/presentation/view/LibrariesTab.kt
@@ -3,12 +3,8 @@ package com.brokenkernel.improvtools.application.presentation.view
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalContext
-import com.mikepenz.aboutlibraries.Libs
 import com.mikepenz.aboutlibraries.ui.compose.m3.LibrariesContainer
-import com.mikepenz.aboutlibraries.util.withContext
 
 @Composable
 internal fun LibrariesTab(onLaunchTitleCallback: () -> Unit) {
