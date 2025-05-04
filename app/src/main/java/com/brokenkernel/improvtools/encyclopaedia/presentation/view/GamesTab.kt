ccd4292be7f43b12471aa34450622d111da37894
Eitan Adler <lists@eitanadler.com>
2025-05-04 01:28:25 -0700

    [encyclopaedia] change UX of bottm sheet to show tag descr

--- app/src/main/java/com/brokenkernel/improvtools/encyclopaedia/presentation/view/GamesTab.kt
+++ app/src/main/java/com/brokenkernel/improvtools/encyclopaedia/presentation/view/GamesTab.kt
@@ -4,17 +4,13 @@ import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.text.input.rememberTextFieldState
-import androidx.compose.foundation.text.input.setTextAndPlaceCursorAtEnd
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Tag
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.ListItem
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.ModalBottomSheet
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
@@ -25,7 +21,6 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.mutableStateSetOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
@@ -39,7 +34,6 @@ import com.brokenkernel.improvtools.encyclopaedia.data.model.GamesDataItem
 import com.brokenkernel.improvtools.encyclopaedia.data.model.GamesDatumTag
 import com.brokenkernel.improvtools.encyclopaedia.data.model.GamesDatumTopic
 import com.brokenkernel.improvtools.encyclopaedia.presentation.viewmodel.GamesTabViewModel
-import kotlinx.coroutines.launch
 
 private fun transformForSearch(str: String): String {
     return str.filterNot { it.isWhitespace() }
@@ -64,8 +58,9 @@ internal fun GamesTab(
     val textFieldState = rememberTextFieldState()
     var shouldShowTagEnabled by remember { mutableStateOf(false) }
     var shouldShowModalBottomSheet by remember { mutableStateOf(false) }
-    val coroutine = rememberCoroutineScope()
     val currentTags = remember { mutableStateSetOf<GamesDatumTag>() }
+    // TODO: this should really be a destination, but deal with it later
+    var currentBottomSheetTag: GamesDatumTag? by remember { mutableStateOf(null) }
 
     fun doesMatch(search: String, gameData: GamesDataItem): Boolean {
         return transformForSearch(gameData.gameName).contains(search, ignoreCase = true) or
@@ -95,8 +90,10 @@ internal fun GamesTab(
         }
     }
 
-    fun doSearchForTag(term: String) {
-        textFieldState.setTextAndPlaceCursorAtEnd(term)
+    // TODO: this should really be passed in as a navigable function
+    fun onNavigateToBottomTag(tag: GamesDatumTag) {
+        shouldShowModalBottomSheet = true
+        currentBottomSheetTag = tag
     }
 
     TabbedSearchableColumn<GamesDatumTopic, GamesDataItem>(
@@ -108,17 +105,12 @@ internal fun GamesTab(
         textFieldState = textFieldState,
         trailingIcon = {
             IconButton(
-                onClick = {
-                    coroutine.launch {
-                        shouldShowModalBottomSheet = true
-                        modalBottomSheetState.partialExpand()
-                    }
-                },
+                onClick = {},
                 enabled = shouldShowTagEnabled,
             ) {
                 Icon(
                     Icons.Default.Tag,
-                    contentDescription = "Tags",
+                    contentDescription = stringResource(R.string.encyclopaedia_tags_filter_button),
                 )
             }
         },
@@ -163,7 +155,7 @@ internal fun GamesTab(
                         it.tags.forEach { tag: GamesDatumTag ->
                             TextButton(
                                 onClick = {
-                                    doSearchForTag(tag.label)
+                                    onNavigateToBottomTag(tag)
                                 },
                             ) {
                                 Text(tag.label)
@@ -182,33 +174,16 @@ internal fun GamesTab(
     }
 
     if (shouldShowModalBottomSheet) {
-//        androidx.compose.material3.ModalBottomSheetDefaults
         ModalBottomSheet(
             onDismissRequest = {
                 shouldShowModalBottomSheet = false
             },
             sheetState = modalBottomSheetState,
         ) {
-            LazyColumn {
-                if (currentTags.isNotEmpty()) {
-                    item {
-                        Text(
-                            stringResource(R.string.encyclopaedia_tags_header),
-                            style = MaterialTheme.typography.headlineLarge,
-                        )
-                    }
-                }
-                items(currentTags.toList(), key = { k -> k }) { tag ->
-                    Text(
-                        tag.label,
-                        style = MaterialTheme.typography.headlineMedium,
-                    )
-                    Text(tag.description)
-                }
+            currentBottomSheetTag?.let {
+                SingleTagBottomTab(it)
             }
         }
     }
-// TODO: make modal bottom sheet a layout
-//    // TODO: maybe bottom sheet should be held by scaffold and moved by navigation
-//
+    // TODO: bottom sheet should be navigated to rather than manually edited
 }
---
+++ app/src/main/java/com/brokenkernel/improvtools/encyclopaedia/presentation/view/SingleTagBottomTab.kt
@@ -0,0 +1,19 @@
+package com.brokenkernel.improvtools.encyclopaedia.presentation.view
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import com.brokenkernel.improvtools.encyclopaedia.data.model.GamesDatumTag
+
+@Composable
+internal fun SingleTagBottomTab(tag: GamesDatumTag) {
+    Column {
+        Text(
+            tag.label,
+            style = MaterialTheme.typography.headlineLarge,
+        )
+        Text(tag.description)
+
+    }
+}
