29943b2baddb129d35452465e913e23bb856097e
Eitan Adler <lists@eitanadler.com>
2025-05-10 19:17:34 -0700

    [app] remove unneeeded abouttab viewmodel

--- app/src/main/java/com/brokenkernel/improvtools/application/utils/AboutTabViewModel.kt
+++
@@ -1,8 +0,0 @@
-package com.brokenkernel.improvtools.application.utils
-
-import androidx.lifecycle.ViewModel
-import dagger.hilt.android.lifecycle.HiltViewModel
-import javax.inject.Inject
-
-@HiltViewModel
-class AboutTabViewModel @Inject constructor() : ViewModel()
