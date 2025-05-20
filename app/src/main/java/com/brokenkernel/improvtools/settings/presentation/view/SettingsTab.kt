cfb703608f963e059ec2a224cac5e856ab156684
Eitan Adler <lists@eitanadler.com>
2025-05-19 21:47:28 -0700

    [settings] simplify settings page, nicer ux
    Prefer ListItem instead of manual 'weight' based Box layout.

--- app/src/main/java/com/brokenkernel/improvtools/settings/presentation/view/SettingsTab.kt
+++ app/src/main/java/com/brokenkernel/improvtools/settings/presentation/view/SettingsTab.kt
@@ -1,14 +1,12 @@
 package com.brokenkernel.improvtools.settings.presentation.view
 
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
+import androidx.compose.material3.ListItem
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Switch
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
@@ -32,76 +30,63 @@ internal fun SettingsTab(viewModel: SettingsScreenViewModel = hiltViewModel()) {
             stringResource(R.string.suggestions_activity_title),
             style = MaterialTheme.typography.titleLarge,
         )
-        Row {
-            Box(modifier = Modifier.weight(.8f)) {
-                Text(
-                    stringResource(R.string.settings_allow_reuse),
-                )
-            }
-            Box {
+        ListItem(
+            headlineContent = { Text(stringResource(R.string.settings_allow_reuse)) },
+            trailingContent = {
                 Switch(
                     checked = uiState.shouldReuseSuggestions,
                     onCheckedChange = {
                         viewModel.onClickUpdateShouldReuseSuggestions(it)
                     },
                 )
-            }
-        }
+            },
+        )
         Text(
             stringResource(R.string.privacy_settings_category),
             style = MaterialTheme.typography.titleLarge,
         )
-        Row {
-            Box(modifier = Modifier.weight(.8f)) {
-                Text(
-                    stringResource(R.string.settings_allow_analytics_cookie_storage),
-                )
-            }
-            Box {
+        ListItem(
+            headlineContent = { Text(stringResource(R.string.settings_allow_analytics_cookie_storage)) },
+            trailingContent = {
                 Switch(
                     checked = uiState.allowAnalyticsCookieStorage,
                     onCheckedChange = {
                         viewModel.onClickUpdateAllowAnalyticsCookieStorage(it)
                     },
                 )
-            }
-        }
+            },
+        )
         Text(
             stringResource(R.string.navigation_tips_and_advice),
             style = MaterialTheme.typography.titleLarge,
         )
-
-        Row {
-            Box(modifier = Modifier.weight(.8f)) {
-                Text(
-                    stringResource(R.string.settings_tips_and_tricks_view_mode),
+        ListItem(
+            headlineContent = { Text(stringResource(R.string.settings_tips_and_tricks_view_mode)) },
+            trailingContent = {
+                EnumerationRadioSelection<TipsAndAdviceViewModeUI, TipsAndTricksViewMode>(
+                    onEnumerationSelection = { opt ->
+                        viewModel.onClickUpdateTipsAndTricksViewMode(opt)
+                    },
+                    currentlySelected = uiState.tipsAndTricksViewMode,
+                    uiToInternalMapping = { opt -> opt.internalEnumMatching },
                 )
-            }
-            EnumerationRadioSelection<TipsAndAdviceViewModeUI, TipsAndTricksViewMode>(
-                onEnumerationSelection = { opt ->
-                    viewModel.onClickUpdateTipsAndTricksViewMode(opt)
-                },
-                currentlySelected = uiState.tipsAndTricksViewMode,
-                uiToInternalMapping = { opt -> opt.internalEnumMatching },
-            )
-        }
+            },
+        )
         Text(
             stringResource(R.string.timer_settings_header),
             style = MaterialTheme.typography.titleLarge,
         )
-        Row {
-            Box(modifier = Modifier.weight(.8f)) {
-                Text(
-                    stringResource(R.string.timer_notifciation_mode),
+        ListItem(
+            headlineContent = { Text(stringResource(R.string.timer_notifciation_mode)) },
+            trailingContent = {
+                EnumerationRadioSelection<TimerHapticsModeUI, TimerHapticsMode>(
+                    onEnumerationSelection = { opt ->
+                        viewModel.onClickUpdateTimerHapticsMode(opt)
+                    },
+                    uiToInternalMapping = { opt -> opt.internalEnumMatching },
+                    currentlySelected = uiState.timerHapticsMode,
                 )
-            }
-            EnumerationRadioSelection<TimerHapticsModeUI, TimerHapticsMode>(
-                onEnumerationSelection = { opt ->
-                    viewModel.onClickUpdateTimerHapticsMode(opt)
-                },
-                uiToInternalMapping = { opt -> opt.internalEnumMatching },
-                currentlySelected = uiState.timerHapticsMode,
-            )
-        }
+            },
+        )
     }
 }
