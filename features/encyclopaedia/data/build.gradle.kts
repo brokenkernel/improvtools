b74d0001bf8aa1caf433161a13c3450481b28b90
Eitan Adler <lists@eitanadler.com>
2025-05-23 21:56:01 -0700

    [build] fix exceptions w.r.t. serialiation
    plugin is required in order to make serialisation work...

--- features/encyclopaedia/data/build.gradle.kts
+++ features/encyclopaedia/data/build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     id("java-library")
     alias(libs.plugins.jetbrains.kotlin.jvm)
     alias(libs.plugins.ksp)
+    alias(libs.plugins.kotlin.serialization)
     id("com.brokenkernel.improvtools.sharedbuildlogic.common-kotlin-plugin")
 }
 
