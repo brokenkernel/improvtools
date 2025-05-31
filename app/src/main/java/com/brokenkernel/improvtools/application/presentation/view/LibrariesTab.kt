2aea7ec81427a0a4cf4da3701ed632b6dd322295
Eitan Adler <lists@eitanadler.com>
2025-05-31 16:51:50 -0700

    [build] update aboutLibraries to latest: 12.2.0

--- app/src/main/java/com/brokenkernel/improvtools/application/presentation/view/LibrariesTab.kt
+++ app/src/main/java/com/brokenkernel/improvtools/application/presentation/view/LibrariesTab.kt
@@ -2,19 +2,24 @@ package com.brokenkernel.improvtools.application.presentation.view
 
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import com.brokenkernel.improvtools.R
 import com.brokenkernel.improvtools.application.navigation.ImprovToolsDestination
 import com.brokenkernel.improvtools.components.sidecar.navigation.ImprovToolsNavigationGraph
+import com.mikepenz.aboutlibraries.ui.compose.android.rememberLibraries
 import com.mikepenz.aboutlibraries.ui.compose.m3.LibrariesContainer
 
 @ImprovToolsDestination<ImprovToolsNavigationGraph>
 @Composable
 internal fun LibrariesTab() {
+    val libraries by rememberLibraries(R.raw.aboutlibraries)
     LibrariesContainer(
         modifier = Modifier.fillMaxSize(),
         showAuthor = true,
         showDescription = true,
         showVersion = true,
         showLicenseBadges = true,
+        libraries = libraries,
     )
 }
