c4b7ca56708953ebbf0a31d9bd42f954bd34de77
Eitan Adler <lists@eitanadler.com>
2025-05-31 23:43:44 -0700

    [encyclopaedia] delete unused ActionsThesaurus class

--- app/src/main/java/com/brokenkernel/improvtools/encyclopaedia/data/model/ActionsThesaurus.kt
+++
@@ -1,9 +0,0 @@
-package com.brokenkernel.improvtools.encyclopaedia.data.model
-
-import com.brokenkernel.improvtools.encyclopaedia.data.DictionaryInfo
-
-internal class ActionsThesaurus(val mergedDictionaryInfo: DictionaryInfo) {
-    fun synonymsForWord(word: String): Set<String> {
-        return mergedDictionaryInfo.synonymsForWord(word)
-    }
-}
