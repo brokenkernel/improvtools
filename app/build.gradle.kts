262be8bc20f7aceb484f7e468fd85b73e4ac944a
Eitan Adler <lists@eitanadler.com>
2025-07-31 21:10:33 -0700

    [fastlane] bump version code to 221

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -59,7 +59,7 @@ android {
         applicationId = "com.brokenkernel.improvtools"
         minSdk = 26
         targetSdk = 36
-        versionCode = 220
+        versionCode = 221
         versionName = "0.0.$versionCode"
 
         testInstrumentationRunner = "com.brokenkernel.improvtools.infrastructure.ImprovToolsTestRunner"
