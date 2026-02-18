e7d9c4e85734dd455b12f0594e1781493745b3ff
Eitan Adler <lists@eitanadler.com>
2026-02-18 04:16:15 -0800

    [build] automated upgrade org.gradle.toolchains.foojay-resolver-convention

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -21,6 +21,7 @@ dependencyResolutionManagement {
 
 plugins {
 //    id("org.jetbrains.kotlinx.kover.aggregation") version "0.9.7"
+    id("org.gradle.toolchains.foojay-resolver-convention") version "1.0.0"
 }
 
 rootProject.name = "ImprovTools"
