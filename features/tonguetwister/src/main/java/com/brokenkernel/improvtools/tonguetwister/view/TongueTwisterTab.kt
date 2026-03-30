8d5b4450c3e1736ed0a6217bbe8c99130811352e
Eitan Adler <lists@eitanadler.com>
2026-03-30 18:08:03 -0400

    [tt] add TTS

--- features/tonguetwister/src/main/java/com/brokenkernel/improvtools/tonguetwister/view/TongueTwisterTab.kt
+++ features/tonguetwister/src/main/java/com/brokenkernel/improvtools/tonguetwister/view/TongueTwisterTab.kt
@@ -1,5 +1,7 @@
 package com.brokenkernel.improvtools.tonguetwister.view
 
+import android.speech.tts.TextToSpeech
+import android.speech.tts.UtteranceProgressListener
 import androidx.compose.foundation.ScrollState
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
@@ -13,20 +15,76 @@ import androidx.compose.foundation.pager.rememberPagerState
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.text.selection.SelectionContainer
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.PauseCircle
+import androidx.compose.material.icons.filled.PlayCircle
 import androidx.compose.material3.Card
 import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.DisposableEffect
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.brokenkernel.components.view.HtmlText
 import com.brokenkernel.components.view.PageDots
+import com.brokenkernel.improvtools.tonguetwister.R
 import com.brokenkernel.improvtools.tonguetwister.data.TongueTwisterDatum
 import com.brokenkernel.improvtools.tonguetwister.data.TongueTwisterItem
 import com.ramcosta.composedestinations.annotation.Destination
 import com.ramcosta.composedestinations.annotation.ExternalModuleGraph
 import com.ramcosta.composedestinations.annotation.parameters.CodeGenVisibility
 import kotlinx.collections.immutable.ImmutableList
+import java.util.Locale
+
+// see https://stackoverflow.com/questions/79493410/text-to-speech-in-jetpack-compose
+@Composable
+private fun rememberTextToSpeech(ttsLocale: Locale): MutableState<TextToSpeech?> {
+    val context = LocalContext.current
+    // TODO: make non-null, get injected?
+    val tts = remember { mutableStateOf<TextToSpeech?>(null) }
+
+    DisposableEffect(context) {
+        val textToSpeech = TextToSpeech(context) { status ->
+            // what to do on failure?
+            if (status == TextToSpeech.SUCCESS) {
+                tts.value?.language = ttsLocale
+            }
+        }
+        tts.value = textToSpeech
+
+        val speechListener = object : UtteranceProgressListener() {
+            override fun onDone(p0: String?) {
+//                TODO("Not yet implemented")
+            }
+
+            override fun onError(p0: String?) {
+//                TODO("Not yet implemented")
+            }
+
+            override fun onStart(p0: String?) {
+//                TODO("Not yet implemented")
+            }
+        }
+        textToSpeech.setOnUtteranceProgressListener(speechListener)
+
+
+        // don't shut down?
+        onDispose {
+            tts.value?.stop()
+            tts.value?.shutdown()
+        }
+    }
+    return tts
+}
 
 @Composable
 private fun SingleTongueTwisterCard(
@@ -42,6 +100,37 @@ private fun SingleTongueTwisterCard(
             Row {
                 SelectionContainer {
                     HtmlText("<big>" + ttitem.asHtmlText() + "</big>")
+                }
+            }
+            Row {
+                // TODO lazy? injected? not sure this is the best.
+                val tts = rememberTextToSpeech(Locale.getDefault()) // TODO // context? something else? per text?
+                var isSpeaking by remember {mutableStateOf(false) }
+                // TODO make toggleButton or PlayPause button?
+                // TODO — ExtendedFloatingActionButton ?
+                IconButton(
+                    onClick = {
+                        if (tts.value?.isSpeaking == true) {
+                            tts.value?.stop()
+                            isSpeaking = true
+                        } else {
+                            tts.value?.speak(ttitem.asRawText(), TextToSpeech.QUEUE_FLUSH, null, null)
+                            isSpeaking = false
+                        }
+                    }
+                ) {
+                    Icon(
+                        imageVector = if (isSpeaking) {
+                            Icons.Default.PauseCircle
+                        } else {
+                            Icons.Default.PlayCircle
+                        },
+                        contentDescription = if (isSpeaking) {
+                            stringResource(R.string.tts_pause)
+                        } else {
+                            stringResource(R.string.tts_listen)
+                        }
+                    )
                 }
             }
             val explanation = ttitem.explanation
