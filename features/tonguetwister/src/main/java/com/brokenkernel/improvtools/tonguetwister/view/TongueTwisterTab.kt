92b61352f86fed6505eb7c6ac02866af67e69cb4
Eitan Adler <lists@eitanadler.com>
2026-04-23 10:26:28 -0700

    [tt] fix locale lint error
    This may not be correct since all TTs are currently English, but I need to better understand what the expected behaviour
    here is.

--- features/tonguetwister/src/main/java/com/brokenkernel/improvtools/tonguetwister/view/TongueTwisterTab.kt
+++ features/tonguetwister/src/main/java/com/brokenkernel/improvtools/tonguetwister/view/TongueTwisterTab.kt
@@ -31,6 +31,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalLocale
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -88,8 +89,9 @@ private fun SingleTongueTwisterCard(
             }
             Row {
                 // TODO lazy? injected? not sure this is the best.
+                // TODO context? something else? per text?
                 val tts =
-                    rememberTextToSpeech(Locale.getDefault()) // TODO // context? something else? per text?
+                    rememberTextToSpeech(LocalLocale.current.platformLocale)
                 var isSpeaking by remember { mutableStateOf(false) }
 
                 val speechListener = object : UtteranceProgressListener() {
