10aa58d7a32f3ac525cc0d511a25d5b8bed03d96
Eitan Adler <lists@eitanadler.com>
2026-04-23 11:13:27 -0700

    [fastlane] bump version code to 241

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -62,7 +62,7 @@ configure<ApplicationExtension> {
         applicationId = "com.brokenkernel.improvtools"
         minSdk = 26
         targetSdk = 37
-        versionCode = 240
+        versionCode = 241
         versionName = "0.0.$versionCode"
 
         testInstrumentationRunner = "com.brokenkernel.improvtools.infrastructure.ImprovToolsTestRunner"
