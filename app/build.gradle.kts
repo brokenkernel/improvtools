c3d10205c727957434f8c16105f1313637623dbc
Eitan Adler <lists@eitanadler.com>
2026-04-23 11:20:32 -0700

    [fastlane] update fastlane

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -62,7 +62,7 @@ configure<ApplicationExtension> {
         applicationId = "com.brokenkernel.improvtools"
         minSdk = 26
         targetSdk = 37
-        versionCode = 241
+        versionCode = 242
         versionName = "0.0.$versionCode"
 
         testInstrumentationRunner = "com.brokenkernel.improvtools.infrastructure.ImprovToolsTestRunner"
