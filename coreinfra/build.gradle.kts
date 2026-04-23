87e4a6e0e7e0b71a45a281bb0759652671ac30ed
Eitan Adler <lists@eitanadler.com>
2026-04-23 10:20:47 -0700

    [build] fix build for kotlin bump for powerassert

--- coreinfra/build.gradle.kts
+++ coreinfra/build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
     alias(libs.plugins.sortDependencies)
     alias(libs.plugins.dependencyAnalysis)
     alias(libs.plugins.ktlint)
-    kotlin("plugin.power-assert") version "2.3.20"
+    kotlin("plugin.power-assert") version "2.3.21"
 }
 
 configure<LibraryExtension> {
