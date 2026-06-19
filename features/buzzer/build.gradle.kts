a7df252da99d38c9cffc8ac33888c1ab2fd034bf
Eitan Adler <lists@eitanadler.com>
2026-06-19 10:44:27 -0700

    [buzzer] add initial buzzer module

---
+++ features/buzzer/build.gradle.kts
@@ -0,0 +1,132 @@
+import com.android.build.api.dsl.LibraryExtension
+import org.jetbrains.kotlin.gradle.ExperimentalKotlinGradlePluginApi
+
+plugins {
+    alias(libs.plugins.android.library)
+    alias(libs.plugins.kotlin.compose)
+    alias(libs.plugins.ksp)
+    alias(libs.plugins.sortDependencies)
+    alias(libs.plugins.dependencyAnalysis)
+    alias(libs.plugins.ktlint)
+    alias(libs.plugins.detekt)
+    kotlin("plugin.power-assert") version libs.versions.kotlin.get()
+}
+
+configure<LibraryExtension> {
+    namespace = "com.brokenkernel.improvtools.buzzer"
+
+    compileSdk = 37
+
+    defaultConfig {
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        consumerProguardFiles("consumer-rules.pro")
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro",
+            )
+        }
+    }
+
+    lint {
+        lintConfig = file("lint.xml")
+        baseline = file("lint-baseline.xml")
+        checkDependencies = true
+        warningsAsErrors = true
+    }
+
+    defaultConfig {
+        minSdk = 26
+        consumerProguardFiles("consumer-rules.pro")
+    }
+
+    buildFeatures {
+        compose = true
+        buildConfig = true
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_21
+        targetCompatibility = JavaVersion.VERSION_21
+    }
+}
+
+composeCompiler {
+    includeSourceInformation = true
+    includeTraceMarkers = true
+    featureFlags = setOf()
+}
+
+kotlin {
+    explicitApi()
+    compilerOptions {
+        allWarningsAsErrors = true
+    }
+}
+dependencies {
+    api(libs.androidx.runtime)
+    api(libs.androidx.ui)
+    api(libs.reorderable)
+
+    debugApi(libs.reorderable.android.debug)
+
+    implementation(project(":components"))
+    implementation(libs.androidx.compose.material.materialIconsExtended)
+    implementation(libs.androidx.foundation)
+    implementation(libs.androidx.foundation.layout)
+    implementation(libs.androidx.lifecycle.common)
+    implementation(libs.androidx.lifecycle.runtime.compose)
+    implementation(libs.androidx.material.icons.core)
+    implementation(libs.androidx.material3)
+    implementation(libs.androidx.ui.graphics)
+    implementation(libs.androidx.ui.text)
+    implementation(libs.androidx.ui.unit)
+    implementation(libs.kotlinx.coroutines.core)
+
+    testImplementation(libs.junit)
+
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.monitor)
+    androidTestImplementation(libs.junit)
+
+    detektPlugins(libs.composeDetektRules)
+    detektPlugins(libs.detektRulesLibraries)
+
+    ktlintRuleset(libs.ktlintCompose)
+
+    lintChecks(libs.androidx.lint.gradle)
+    lintChecks(libs.slack.lint.checks)
+}
+
+@OptIn(ExperimentalKotlinGradlePluginApi::class)
+powerAssert {
+    // have to list them all, since "detect all" doesn't work with android
+    includedSourceSets = listOf(
+        "debug",
+        "debugAndroidTest",
+        "debugUnitTest",
+        "release",
+        "releaseUnitTest",
+    )
+}
+
+ksp {
+    arg("compose-destinations.mermaidGraph", "$rootDir/docs/static/")
+    arg("compose-destinations.htmlMermaidGraph", "$rootDir/docs/static/")
+    allWarningsAsErrors = true
+    arg("dagger.useBindingGraphFix", "enabled")
+    arg("dagger.ignoreProvisionKeyWildcards", "enabled")
+    arg("dagger.experimentalDaggerErrorMessages", "enabled")
+    arg("dagger.warnIfInjectionFactoryNotGeneratedUpstream", "enabled")
+    arg("dagger.fullBindingGraphValidation", "error")
+}
+
+ktlint {
+    android.set(true)
+    coloredOutput.set(true)
+    version.set("1.8.0")
+}
