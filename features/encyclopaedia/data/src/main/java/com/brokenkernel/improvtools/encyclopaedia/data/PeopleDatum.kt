c429258ce039215f3abff62620dd57c2942078a5
Eitan Adler <lists@eitanadler.com>
2026-06-28 12:02:49 -0700

    [encyclopaedia] add more people. add links

--- features/encyclopaedia/data/src/main/java/com/brokenkernel/improvtools/encyclopaedia/data/PeopleDatum.kt
+++ features/encyclopaedia/data/src/main/java/com/brokenkernel/improvtools/encyclopaedia/data/PeopleDatum.kt
@@ -2,7 +2,6 @@ package com.brokenkernel.improvtools.encyclopaedia.data
 
 import java.net.URI
 
-// TODO: add link to wikipedia for others
 // TODO: localise wikipedia link
 // TODO: figure out how to open a specific item at start, or deep link, or similar. For example filter to 'BATS' or some such
 // TODO: make things internal again possible
@@ -57,14 +56,55 @@ public val PeopleDatum: Collection<PeopleDataItem> = listOf(
         "The Second City",
         "https://en.wikipedia.org/wiki/Paul_Sills".toUri(),
     ),
-    PeopleDataItem("Susan Messing", PeopleDatumTopic.PERSON, "Annoyance Theater and iO Theater"),
-    PeopleDataItem("Rafe Chase", PeopleDatumTopic.PERSON, "BATS Improv"),
-    PeopleDataItem("Armando Diaz", PeopleDatumTopic.PERSON, "Magnet Theatre"),
-    PeopleDataItem("Rebecca Stockley", PeopleDatumTopic.PERSON, "BATS Improv"),
-    PeopleDataItem("Stephen Kerrin", PeopleDatumTopic.PERSON, "BATS Improv"),
-    PeopleDataItem("Tim Orr", PeopleDatumTopic.PERSON, "BATS Improv"),
-    PeopleDataItem("Gary Austin", PeopleDatumTopic.PERSON, "BATS Improv"),
-    PeopleDataItem("Konstantin Stanislavski", PeopleDatumTopic.PERSON, ""),
+    PeopleDataItem(
+        "Susan Messing",
+        PeopleDatumTopic.PERSON,
+        "Annoyance Theater and iO Theater",
+        learnMoreLink = "https://en.wikipedia.org/wiki/Susan_Messing".toUri(),
+    ),
+    PeopleDataItem(
+        "Rafe Chase",
+        PeopleDatumTopic.PERSON,
+        "BATS Improv",
+    ),
+    PeopleDataItem(
+        "Armando Diaz",
+        PeopleDatumTopic.PERSON,
+        "Magnet Theatre",
+        learnMoreLink = "https://en.wikipedia.org/wiki/Armando_Diaz".toUri(),
+    ),
+    PeopleDataItem(
+        "Rebecca Stockley",
+        PeopleDatumTopic.PERSON,
+        "BATS Improv"
+    ),
+    PeopleDataItem(
+        "Stephen Kerrin",
+        PeopleDatumTopic.PERSON,
+        "BATS Improv"
+    ),
+    PeopleDataItem(
+        "Tim Orr",
+        PeopleDatumTopic.PERSON,
+        "BATS Improv"
+    ),
+    PeopleDataItem(
+        "Gary Austin",
+        PeopleDatumTopic.PERSON,
+        "BATS Improv"
+    ),
+    PeopleDataItem(
+        "Konstantin Stanislavski",
+        PeopleDatumTopic.PERSON,
+        "",
+        learnMoreLink = "https://en.wikipedia.org/wiki/Konstantin_Stanislavski".toUri(),
+    ),
+    PeopleDataItem(
+        personName = "Sanford Meisner",
+        topic = PeopleDatumTopic.PERSON,
+        knownFor = "Meisner Technique",
+        learnMoreLink = "https://en.wikipedia.org/wiki/Sanford_Meisner".toUri(),
+    ),
     PeopleDataItem(
         "BATS Improv",
         PeopleDatumTopic.TROOP,
@@ -102,11 +142,51 @@ public val PeopleDatum: Collection<PeopleDataItem> = listOf(
         "Theatre Games",
         "https://en.wikipedia.org/wiki/Clive_Barker_(editor)".toUri(),
     ),
-    PeopleDataItem("David Shepherd", PeopleDatumTopic.PERSON, "The Compass Players"),
-    PeopleDataItem("Elaine May", PeopleDatumTopic.PERSON, "comedic improv"),
-    PeopleDataItem("Alan Myerson", PeopleDatumTopic.PERSON, "The Committee"),
-    PeopleDataItem("Augusto Boal", PeopleDatumTopic.PERSON, "Theatre of the Oppressed"),
-    PeopleDataItem("Ruth Zaporah", PeopleDatumTopic.PERSON, "Action Theatre"),
-    PeopleDataItem("William Hall", PeopleDatumTopic.PERSON, "BATS Improv"),
-    PeopleDataItem("Brave New Workshop", PeopleDatumTopic.TROOP, "Oldest US comedy theater"),
+    PeopleDataItem(
+        "David Shepherd",
+        PeopleDatumTopic.PERSON,
+        "The Compass Players"
+    ),
+    PeopleDataItem(
+        "Elaine May",
+        PeopleDatumTopic.PERSON,
+        "comedic improv",
+        learnMoreLink = "https://en.wikipedia.org/wiki/Elaine_May".toUri(),
+    ),
+    PeopleDataItem(
+        "Alan Myerson",
+        PeopleDatumTopic.PERSON,
+        "The Committee",
+        learnMoreLink = "https://en.wikipedia.org/wiki/Alan_Myerson".toUri(),
+    ),
+    PeopleDataItem(
+        "Augusto Boal",
+        PeopleDatumTopic.PERSON,
+        "Theatre of the Oppressed",
+        learnMoreLink = "https://en.wikipedia.org/wiki/Augusto_Boal".toUri(),
+    ),
+    PeopleDataItem(
+        personName = "https://en.wikipedia.org/wiki/Theatre_of_the_Oppressed",
+        topic = PeopleDatumTopic.TROOP,
+        knownFor = "",
+        learnMoreLink = "https://en.wikipedia.org/wiki/Theatre_of_the_Oppressed".toUri(),
+    ),
+    PeopleDataItem(
+        "Ruth Zaporah",
+        PeopleDatumTopic.PERSON,
+        "Action Theatre",
+        learnMoreLink = "https://en.wikipedia.org/wiki/Action_Theatre_(Ruth_Zaporah)".toUri(),
+    ),
+    PeopleDataItem(
+        "William Hall",
+        PeopleDatumTopic.PERSON,
+        "BATS Improv",
+        learnMoreLink = "https://en.wikipedia.org/wiki/William_Hall_(actor)".toUri(),
+    ),
+    PeopleDataItem(
+        "Brave New Workshop",
+        PeopleDatumTopic.TROOP,
+        "Oldest US comedy theater",
+        learnMoreLink = "https://en.wikipedia.org/wiki/Brave_New_Workshop".toUri(),
+    ),
 )
