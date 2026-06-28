7e44bd001e54726145cd3ed84482d98ad051f17b
Eitan Adler <lists@eitanadler.com>
2026-06-28 15:53:01 -0700

    [suggestions] fix module name for destinations

--- features/suggestions/android/build.gradle.kts
+++ features/suggestions/android/build.gradle.kts
@@ -118,7 +118,7 @@ powerAssert {
 }
 
 ksp {
-    arg("compose-destinations.moduleName", project.name)
+    arg("compose-destinations.moduleName", "suggestions")
     arg("compose-destinations.mermaidGraph", "$rootDir/docs/static/")
     arg("compose-destinations.htmlMermaidGraph", "$rootDir/docs/static/")
     allWarningsAsErrors = true
