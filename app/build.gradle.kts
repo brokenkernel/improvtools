1bd80c3aa7bc99973c33c1c2e71cb4ff752f3ea2
Eitan Adler <lists@eitanadler.com>
2026-07-02 00:05:15 -0700

    [fastlane] bump version code to 260

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -63,7 +63,7 @@ configure<ApplicationExtension> {
         applicationId = "com.brokenkernel.improvtools"
         minSdk = 26
         targetSdk = 37
-        versionCode = 259
+        versionCode = 260
         versionName = "0.0.$versionCode"
 
         testInstrumentationRunner =
