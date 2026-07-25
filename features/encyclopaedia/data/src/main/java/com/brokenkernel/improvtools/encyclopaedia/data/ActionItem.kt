8f86b057134575eb104511ad6070116ec5a4366d
Eitan Adler <lists@eitanadler.com>
2026-07-24 19:34:18 -0700

    [encyclopaedia] add unused plural marker for now

--- features/encyclopaedia/data/src/main/java/com/brokenkernel/improvtools/encyclopaedia/data/ActionItem.kt
+++ features/encyclopaedia/data/src/main/java/com/brokenkernel/improvtools/encyclopaedia/data/ActionItem.kt
@@ -6,4 +6,9 @@ import kotlinx.collections.immutable.persistentSetOf
 public data class ActionItem(
     val word: String,
     val synonyms: ImmutableSet<String> = persistentSetOf(),
+
+    /**
+     * The word is best used when played to a group of people
+     */
+    val plural: Boolean = false
 )
